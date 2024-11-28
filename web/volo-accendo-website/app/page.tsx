f081bf50e359bffe42dde88af263912e50b6553a
Ivan Stashak <istashak@cardinalpeak.com>
2024-11-28 11:03:03 -0700

    Config changes and other odds and ends

--- web/volo-accendo-website/app/page.tsx
+++ web/volo-accendo-website/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <div>Home</div>
+      <div>Volo Accendo Inc.'s Home Page</div>
       <div>{process.env.VOLO_ACCENDO_API}</div>
     </div>
   );
