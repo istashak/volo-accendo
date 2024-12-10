f8a747c52774361b2f7b884c46497aaa25b491b9
Ivan Stashak <istashak@cardinalpeak.com>
2024-12-10 11:41:43 -0700

    Static contact verification page

---
+++ web/volo-accendo-website/app/contact/verification/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <div>
+        <p className="mb-4 font-semibold">{`Thank you've successfully verified your email address.`}</p>
+      </div>
+    </>
+  );
+}
