786795aa8edf234c888f34325f8146e890ef6da8
Ivan Stashak <istashak@cardinalpeak.com>
2024-12-11 10:19:33 -0700

    Terraform debugging for SES

--- cloud/apis/contacts-api/terraform/ses.tf
+++ cloud/apis/contacts-api/terraform/ses.tf
@@ -18,14 +18,11 @@ resource "aws_ses_domain_dkim" "contact_verification_dkim" {
   domain = aws_ses_domain_identity.contact_verification_domain.domain
 }
 
-resource "aws_route53_record" "dkim" {
-  #   count   = length(aws_ses_domain_dkim.contact_verification_dkim.dkim_tokens)
-  for_each = toset(try(aws_ses_domain_dkim.contact_verification_dkim.dkim_tokens, ["foo1"]))
-  zone_id  = data.tfe_outputs.networking.nonsensitive_values.domain_zone_id
-  #   name     = "${aws_ses_domain_dkim.contact_verification_dkim.dkim_tokens[count.index]}._domainkey.${local.domain_name}"
-  name = "${each.value}._domainkey.${local.domain_name}"
-  type = "CNAME"
-  ttl  = 300
-  #   records = ["${aws_ses_domain_dkim.contact_verification_dkim.dkim_tokens[count.index]}.amazonses.com"]
-  records = ["${each.value}.amazonses.com"]
-}
+# resource "aws_route53_record" "dkim" {
+#   for_each = toset(try(aws_ses_domain_dkim.contact_verification_dkim.dkim_tokens, []))
+#   zone_id  = data.tfe_outputs.networking.nonsensitive_values.domain_zone_id
+#   name = "${each.value}._domainkey.${local.domain_name}"
+#   type = "CNAME"
+#   ttl  = 300
+#   records = ["${each.value}.amazonses.com"]
+# }
