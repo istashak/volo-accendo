11290da872249de4ddab17852c0e2c06a78babb7
Ivan Stashak <istashak@cardinalpeak.com>
2025-01-09 10:59:38 -0700

    Ordered cache behavior test

--- cloud/terraform/cloudfront.tf
+++ cloud/terraform/cloudfront.tf
@@ -40,27 +40,27 @@ resource "aws_cloudfront_distribution" "web_app_distribution" {
   default_root_object = "index.html"
 
   # Priority 0
-  ordered_cache_behavior {
-    path_pattern     = "_next/static/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = aws_s3_bucket.web_app_bucket.id
+  # ordered_cache_behavior {
+  #   path_pattern     = "_next/static/*"
+  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+  #   cached_methods   = ["GET", "HEAD", "OPTIONS"]
+  #   target_origin_id = aws_s3_bucket.web_app_bucket.id
 
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
+  #   forwarded_values {
+  #     query_string = false
+  #     headers      = ["Origin"]
 
-      cookies {
-        forward = "none"
-      }
-    }
+  #     cookies {
+  #       forward = "none"
+  #     }
+  #   }
 
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
+  #   min_ttl                = 0
+  #   default_ttl            = 86400
+  #   max_ttl                = 31536000
+  #   compress               = true
+  #   viewer_protocol_policy = "redirect-to-https"
+  # }
 
   # # Priority 1
   # ordered_cache_behavior {
