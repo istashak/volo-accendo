461d954e66eaec1ceecf67a67fa71e1e795785b1
Ivan Stashak <istashak@cardinalpeak.com>
2025-01-28 09:54:25 -0700

    lambda@edge troubleshooting 24

--- web/volo-accendo-website/next.config.mjs
+++ web/volo-accendo-website/next.config.mjs
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
+  compress: false,
   reactStrictMode: true,
   swcMinify: true,
   // output: "export",
