d11eefa8773015810fb8a958042e8d925a904896
Ivan Stashak <istashak@cardinalpeak.com>
2025-02-06 14:59:55 -0700

    Improved route rewrite logic vii

--- cloud/terraform/path-rewrite-function.js
+++ cloud/terraform/path-rewrite-function.js
@@ -9,8 +9,9 @@ function handler(event) {
     // Load static routes from the routesManifest
     var staticRoutes = routesManifest.staticRoutes.map(route => route.page);
 
-    for (var route of staticRoutes) {
-        console.log("looking at route", route)
+    for (var i = 0; i < staticRoutes.length; i++) {
+        var route = staticRoutes[i];
+        console.log("looking at route", route);
         if (uri === route || uri === route + "/") {
             request.uri = route + ".html";
             return request;
