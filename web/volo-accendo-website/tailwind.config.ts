ecb8389ec2efd62efc3dd653f0d0dc497326fa26
Ivan Stashak <istashak@cardinalpeak.com>
2025-02-07 06:31:43 -0700

    Improved route rewrite logic ix

--- web/volo-accendo-website/tailwind.config.ts
+++ web/volo-accendo-website/tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      minWidth: {
+        'navbar': '320px', // You can adjust this value as needed
+      },
     },
   },
   plugins: [require("daisyui")],
